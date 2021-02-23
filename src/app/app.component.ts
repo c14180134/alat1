672e65c23e0eddb0000eee14e9e2d4f46693f867
c14180134 <c14180134@john.petra.ac.id>
2021-02-23 18:02:39 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  baris=0;
+  kolom=0;
+
+  no1="*";
+  no2="*";
+  no3="*";
+  no4="*";
+  no5="*";
+
+  no6="*";
+  no7="*";
+  no8="*";
+  no9="*";
+  no10="*";
+  
+  no11="*";
+  no12="*";
+  no13="*";
+  no14="*";
+  no15="*";
+
+  no16="*";
+  no17="*";
+  no18="*";
+  no19="*";
+  no20="*";
+  
+  no21="*";
+  no22="*";
+  no23="*";
+  no24="*";
+  no25="*";
 }
